d8e5b07a109c52d386f468934292b816b91f4291
unknown <andres.solla@gmail.com>
2021-03-01 17:51:04 -0500

    first commit

---
+++ src/main/java/com/soya/HeavensPizzaApi/HeavensPizzaApiApplication.java
@@ -0,0 +1,13 @@
+package com.soya.HeavensPizzaApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavensPizzaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeavensPizzaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soya/HeavensPizzaApi/HeavensPizzaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soya.HeavensPizzaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeavensPizzaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
